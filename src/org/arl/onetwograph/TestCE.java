5cb0d2301dc7d3be1638168b582da0db0bf53771
jeffhoye <jeffhoye@gmail.com>
2015-08-06 20:23:07 -0400

    merge cestore

---
+++ src/org/arl/onetwograph/TestCE.java
@@ -0,0 +1,15 @@
+package org.arl.onetwograph;
+
+import com.ibm.ets.ita.ce.store.*;
+import com.ibm.ets.ita.ce.store.api.*;
+
+public class TestCE {
+
+	public static void main(String[] args) {
+		StoreActions store = 
+//		CEStore store = new StoreActions();
+		// TODO Auto-generated method stub
+
+	}
+
+}
